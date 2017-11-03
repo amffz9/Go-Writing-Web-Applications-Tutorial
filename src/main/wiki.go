39151c80b321bf90474afdc77d0d10eec11656ef
aforbis <aforbis83091@gmail.com>
2017-11-03 14:03:51 -0500

    This is a start of a new section to edit pages, we could load the pages into an html string, or we could ....

--- src/main/wiki.go
+++ src/main/wiki.go
@@ -14,6 +14,48 @@ type Page struct {
 	Body  []byte
 }
 
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
+}
+func saveHandler(response http.ResponseWriter, request *http.Request) {
+
+}
+
+//edit handler loads the page(or an empty one if it doesn't exist) and displays an html form
+func editHandler(response http.ResponseWriter, request *http.Request) {
+	title := request.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(response, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+
+}
+
+func viewHandler(response http.ResponseWriter, request *http.Request) {
+	//Get everything in the path after the first part of the path, which will be "/view/"
+	//and put it into title
+	title := request.URL.Path[len("/view/"):]
+
+	//We use the title to load an existing page
+	//we are ignoring errors for now
+	page, _ := loadPage(title)
+
+	//Then we put the page information into an html string then write in out to our response
+	fmt.Fprintf(response, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
+
+}
+
 //save operates on a page type and saves it to disk, using the page title as the filename
 //and saving the body of the page as the file content
 func (p *Page) save() error {
@@ -34,23 +76,4 @@ func loadPage(title string) (*Page, error) {
 	}
 
 	return &Page{Title: title, Body: body}, nil
-}
-
-func viewHandler(response http.ResponseWriter, request *http.Request) {
-	//Get everything in the path after the first part of the path, which will be "/view/"
-	//and put it into title
-	title := request.URL.Path[len("/view/"):]
-
-	//We use the title to load an existing page
-	//we are ignoring errors for now
-	page, _ := loadPage(title)
-
-	//Then we put the page information into an html string then write in out to our response
-	fmt.Fprintf(response, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
-
-}
-
-func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.ListenAndServe(":8080", nil)
 }
